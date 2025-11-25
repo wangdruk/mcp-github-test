c466d35ac8f88247988d0c5b9f2745b7e2217275
Wangdruk <bhutan.infotech@gmail.com>
2025-11-25 21:19:25 +1000

    def add_numbers(a, b):     return a + b
    result = add_numbers(3, 5)
    print("The sum is:", result)

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
-py
-// Function to add two numbers
+#Function to add two numbers
+def add(a, b):
+    return a + b    
+#Function to subtract two numbers
+def subtract(a, b):
+    return a - b
+#Function to multiply two numbers
+def multiply(a, b):
+    return a * b    
+
